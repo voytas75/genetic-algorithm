469a7c9f1f002a7b46d9d6a1a3cccae6c549537f
Wojciech Napierała <wnapierala@hotmail.com>
2020-08-08 18:22:17 +0200

    psm and manifest

---
+++ Start-GA.psm1
@@ -0,0 +1,11 @@
+#Get files.
+$files  = @( Get-ChildItem -Path $PSScriptRoot\*.ps1 -ErrorAction SilentlyContinue )
+#Dot source the files
+Foreach ($import in @($files)) {
+    Try {
+        . $import.fullname
+    }
+    Catch {
+        Write-Error -Message "Failed to import file $($import.fullname): $_"
+    }
+}
